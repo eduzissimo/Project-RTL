e0abc2c3e00ea231b2928c28b5e329e17ad22e28
eduzissimo <eduardo.alves14@hotmail.com>
2023-11-18 12:47:22 -0300

    Feat: reup dos testes para Fav. Pokémon e Not Found

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,25 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Verifica se na ausência de pokémons favoritos é exibida a mensagem "No favorite pokemon found', () => {
+  renderWithRouter(<App />, { route: '/favorites' });
+  const noFavoritePokemons = screen.getByText(/no favorite pokémon found/i);
+  expect(noFavoritePokemons).toBeInTheDocument();
+});
+
+test('Verifica se os cards com os pokémons favoritados são exibidos na tela', async () => {
+  const { user } = renderWithRouter(<App />);
+
+  const pokemonsDetails = screen.getByRole('link', { name: /more details/i });
+  await user.click(pokemonsDetails);
+
+  const favPokemon = screen.getByRole('checkbox', { name: /pokémon favoritado/i });
+  await user.click(favPokemon);
+
+  const favoritePokemonLink = screen.getByRole('link', { name: /favorite pokémon/i });
+  await user.click(favoritePokemonLink);
+
+  const pikachuData = screen.getByText(/pikachu/i);
+  expect(pikachuData).toBeInTheDocument();
+});
--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,16 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Verifica se a página possui o heading "Page requested not found"', () => {
+  renderWithRouter(<App />, { route: '/pagina-que-nao-existe' });
+  const notFound = screen.getByRole('heading', { name: /Page requested not found/i });
+  expect(notFound).toBeInTheDocument();
+});
+
+test('Verifica se a página exite o texto alternativo', () => {
+  renderWithRouter(<App />, { route: '/pagina-que-nao-existe' });
+  const notFound = screen.getByAltText(/Clefairy pushing buttons randomly with text I have no idea what i'm doing/i);
+  const imgSource = '/404.gif';
+  expect(notFound).toHaveAttribute('src', imgSource);
+});
