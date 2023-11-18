2e7fae969462142ad74a344c50a1010e899273b2
eduzissimo <eduardo.alves14@hotmail.com>
2023-11-18 13:36:13 -0300

    Feat: finalizando o projeto de testes.

--- src/tests/Pokemon.test.tsx
+++ src/tests/Pokemon.test.tsx
@@ -1,1 +1,45 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Verifica se na página é renderizado um card com as informações de deteminado Pokémon', () => {
+  renderWithRouter(<App />);
+
+  const pokemonName = screen.getByTestId('pokemon-name');
+  const pokemonType = screen.getByTestId('pokemon-type');
+  const pokemonWeight = screen.getByTestId('pokemon-weight');
+  const pokemonImage = screen.getByRole('img');
+  const imgSrc = 'https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png';
+  const imgSrcAlt = 'Pikachu sprite';
+
+  expect(pokemonName.textContent).toEqual('Pikachu');
+  expect(pokemonType.textContent).toEqual('Electric');
+  expect(pokemonWeight.textContent).toEqual('Average weight: 6.0 kg');
+  expect(pokemonImage).toHaveAttribute('src', imgSrc);
+  expect(pokemonImage).toHaveAttribute('alt', imgSrcAlt);
+});
+
+test('Verifica se na página do Pokémon possui um link de navegação para exibir detalhes', async () => {
+  const { user } = renderWithRouter(<App />);
+
+  const detailsLink = screen.getByRole('link', { name: /more details/i });
+  expect(detailsLink).toBeInTheDocument();
+  expect(detailsLink).toHaveAttribute('href', '/pokemon/25');
+  await user.click(detailsLink);
+  expect(screen.getByText(/Summary/i)).toBeInTheDocument();
+});
+
+test('Verifica se existe um ícone de estrela nos pokémons favoritados', async () => {
+  const { user } = renderWithRouter(<App />);
+
+  const detailsLink = screen.getByRole('link', { name: /more details/i });
+  await user.click(detailsLink);
+  expect(window.location.pathname).toBe('/pokemon/25');
+  expect(screen.getByText(/Summary/i)).toBeInTheDocument();
+
+  const favoritePokemon = screen.getByRole('checkbox', { name: /pokémon favoritado/i });
+  await user.click(favoritePokemon);
+
+  const icon = screen.getByAltText('Pikachu is marked as favorite');
+  expect(icon).toBeInTheDocument();
+});
--- src/tests/PokemonDetails.test.tsx
+++ src/tests/PokemonDetails.test.tsx
@@ -1,1 +1,53 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+const pikachuData = '/pokemon/25';
+const snorlaxData = '/pokemon/143';
+
+test('Verifica se as informações detalhadas do pokémon são mostradas na tela', () => {
+  renderWithRouter(<App />, { route: pikachuData });
+
+  const pokeName = screen.getByRole('heading', { level: 2, name: /pikachu details/i });
+  const pokeLink = screen.queryByRole('link', { name: /more details/i });
+  const summaryTitle = screen.getByRole('heading', { level: 2, name: /summary/i });
+  const pokeSummary = screen.getByText('This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.');
+
+  expect(pokeName).toBeInTheDocument();
+  expect(pokeLink).not.toBeInTheDocument();
+  expect(summaryTitle).toBeInTheDocument();
+  expect(pokeSummary).toBeInTheDocument();
+});
+
+test('Verifica se existe na página uma seção com os mapas contendo as localizações do pokémon', () => {
+  renderWithRouter(<App />, { route: snorlaxData });
+
+  const mapLocationURL = 'https://archives.bulbagarden.net/media/upload/5/54/Kanto_Vermilion_City_Map.png';
+  const mapLocationTitle = screen.getByRole('heading', { level: 2, name: /game locations of snorlax/i });
+  const mapLocation = screen.getByText(/kanto vermillion city/i);
+  const snorlaxLoc = screen.getByRole('img', { name: /snorlax location/i });
+
+  expect(mapLocationTitle).toBeInTheDocument();
+  expect(mapLocation).toBeInTheDocument();
+  expect(snorlaxLoc).toBeInTheDocument();
+  expect(snorlaxLoc).toHaveAttribute('src', mapLocationURL);
+});
+
+test('Verifica se por meio da página de detalhes é possível favoritar um Pokémon', async () => {
+  const { user } = renderWithRouter(<App />, { route: snorlaxData });
+
+  const favButton = screen.getByRole('checkbox', { name: /pokémon favoritado/i });
+  const favMenu = screen.getByRole('link', { name: /favorite pokémon/i });
+  expect(favButton).toBeInTheDocument();
+
+  await user.click(favButton);
+  await user.click(favMenu);
+
+  const snorlax = screen.queryByText(/snorlax/i);
+  expect(snorlax).toBeInTheDocument();
+
+  const moreDetails = screen.getByRole('link', { name: /more details/i });
+  await user.click(moreDetails);
+  await user.click(favButton);
+  await user.click(favMenu);
+});
